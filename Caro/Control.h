97506d25588c3ba70590baea4b51de9a059fcc6b
Nhan <minecraft.nhan925@gmail.com>
2023-04-19 17:28:42 +0700

    Delete duplicate function prototype

--- Caro/Control.h
+++ Caro/Control.h
@@ -17,11 +17,9 @@
 #include "About.h"
 
 void StartGame();
-void HandleKeyForBoard(int x, int y, KEY_EVENT_RECORD key);
 void HandleEvent(int x, int y, void(*func)(int, int, KEY_EVENT_RECORD));
 void Exit();
 void ControlMenu();
-
 void InitGame();
 
 #endif
