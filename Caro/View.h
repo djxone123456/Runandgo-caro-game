b26a3e9244728a66a0c0b60f7e1cb44b78304288
Nhan <minecraft.nhan925@gmail.com>
2023-04-19 17:29:32 +0700

    Delete duplicate function prototype

--- Caro/View.h
+++ Caro/View.h
@@ -6,7 +6,6 @@
 
 void CreateConsoleWindow(int width, int height);
 void GotoXY(int x, int y);
-void DrawBoard(int pSize);
 void ShowCur(bool CursorVisibility);
 void PrintTextColor(const wstring& s, const int& color);
 void PrintTextColor(const string& s, const int& color);
